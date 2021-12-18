d26d9b06d479ee5b3b17bbc2852670820bc0e37e
Alzhikkkk <alzhikakh@gmail.com>
2021-12-18 21:50:12 +0600

    Changes

--- js/search.js
+++ js/search.js
@@ -1,20 +1,25 @@
 let arr = ["Welcome","Home", "Shop","Travel","Contact"];
 let searchInput = document.getElementById("input");
+res = document.getElementById("result");
 
 function showTotal() {
     let fitered = arr.filter(item => 
       item.includes(searchInput.value));
-      res = document.getElementById("result");
+     
       res.innerHTML = '';
       let list = '';
       for (i=0; i<fitered.length; i++) {
         list += '<li onclick="replaceValue(this)">' + fitered[i] + '</li>';
       }
       res.innerHTML = '<ul>' + list + '</ul>';
+      if(searchInput === document.activeElement){
+        res.style.display="block";
+    }
   }
 
   function replaceValue(item){
     searchInput.value=item.innerText;
+    res.style.display="none"
   }
 
 
